f77f38a616adc0ca20d5f2a34a31714d73736916
James <nmfuntouchable@gmail.com>
2020-05-08 09:57:05 -1000

    removed piechart3d

--- src/app/modules/charts/charts.module.ts
+++ src/app/modules/charts/charts.module.ts
@@ -3,17 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { LineChartComponent } from './components/line-chart/line-chart.component';
 import { PieChartComponent } from './components/pie-chart/pie-chart.component';
-import { PieThreeDComponent } from './components/pie-three-d/pie-three-d.component';
 
 @NgModule({
   declarations: [
-    LineChartComponent, 
-    PieChartComponent, PieThreeDComponent
+    LineChartComponent,
+    PieChartComponent,
   ],
   exports: [
-    LineChartComponent, 
-    PieChartComponent,
-    PieThreeDComponent
+    LineChartComponent,
+    PieChartComponent
   ],
   imports: [
     CommonModule
