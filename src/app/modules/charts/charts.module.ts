d3c92cff8eea7f98db55d1e9f1ba38fdd12ed808
jhutch42 <jhutch42@hawaii.edu>
2021-11-16 12:36:52 -1000

    Removed 3d Pie chart

--- src/app/modules/charts/charts.module.ts
+++ src/app/modules/charts/charts.module.ts
@@ -4,7 +4,6 @@ import { GoogleChartsModule } from 'angular-google-charts';
 
 import { LineChartComponent } from './components/line-chart/line-chart.component';
 import { PieChartComponent } from './components/pie-chart/pie-chart.component';
-import { PieThreeDComponent } from './components/pie-three-d/pie-three-d.component';
 import { GagueChartComponent } from './gague-chart/gague-chart.component';
 import { BarComponent } from './components/bar/bar.component';
 
@@ -13,14 +12,12 @@ import { BarComponent } from './components/bar/bar.component';
   declarations: [
     LineChartComponent,
     PieChartComponent,
-    PieThreeDComponent,
     GagueChartComponent,
     BarComponent
   ],
   exports: [
     LineChartComponent,
     PieChartComponent,
-    PieThreeDComponent,
     GagueChartComponent,
     BarComponent
   ],
